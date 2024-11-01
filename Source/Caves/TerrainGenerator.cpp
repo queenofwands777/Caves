c87827dce0a520454b2d8a643d8837561b78cea2
queenofwands777 <beccamac777@gmail.com>
2024-10-31 23:23:49 -0400

    completed arms system, now I just need to plug in all the weapons. still need to make the arms the right color, as well as develop systems for sprite-based shoulder/hand positioning.

--- Source/Caves/TerrainGenerator.cpp
+++ Source/Caves/TerrainGenerator.cpp
@@ -530,7 +530,7 @@ void ATerrainGenerator::GenerateMap() {
 
 	//create spawn area
 	SetTile(cursor_x, cursor_y, floor_material, 7);
-	FVector spawn_location = { float(cursor_x * TILE_WIDTH) + (1 * TILE_WIDTH), 2, float(cursor_y * TILE_HEIGHT) - (16 * TILE_HEIGHT) };
+	FVector spawn_location = { float(cursor_x * TILE_WIDTH) + (1 * TILE_WIDTH), 4, float(cursor_y * TILE_HEIGHT) - (16 * TILE_HEIGHT) };
 	FRotator spawn_rotation = { 0,0,0 };
 	GetWorld()->SpawnActor<AActor>(Player, spawn_location, spawn_rotation);
 	FVector portal_location = { float(cursor_x * TILE_WIDTH) - (2 * TILE_WIDTH), 1.9, float(cursor_y * TILE_HEIGHT) - (16 * TILE_HEIGHT) };
