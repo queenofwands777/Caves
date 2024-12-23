cf8e04ddf64b07ae1d86725c9a08fb33bb4a070f
Becca <Becca@DESKTOP-45HH06R>
2024-12-23 14:02:44 -0500

    Working on lightning gun

--- Source/Caves/Private/LightningBolt.cpp
+++ Source/Caves/Private/LightningBolt.cpp
@@ -28,5 +28,32 @@ void ALightningBolt::Tick(float DeltaTime)
 }
 
 void ALightningBolt::DrawLightning() {
-	DrawDebugLine(GetWorld(), start_location, end_location, FColor::Blue, false, 0.1, 255, 1);
+
+	TArray<FVector> points;
+
+	FVector displacement = end_location - start_location;
+	FVector direction = displacement;
+	direction.Normalize();
+
+	float distance = FVector::Distance(start_location, end_location);
+	float num_breaks = 10;
+	float segment_length = distance / num_breaks;
+
+
+	for (int i = 0; i < num_breaks + 1; i++) {
+		points.Add(start_location + (direction * i * segment_length));
+	}
+
+	FVector perpendicular = { -direction[2], direction[1], direction[0] };
+	for (int i = 1; i < points.Num()-1; i++) {
+		float point_displacement = FMath::FRandRange(-10.0, 10.0);
+		points[i] += perpendicular * point_displacement;
+	}
+
+	for (int i = 0; i < points.Num() - 1; i++) {
+		DrawDebugLine(GetWorld(), points[i], points[i+1], FColor::Cyan, false, 0.1, 255, 1);
+
+	}
+
+
 }
